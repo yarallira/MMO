fa36cc753c8d366e0417ac81544f1fc4fbd36ebc
yarallira <yarallira555@gmail.com>
2020-01-17 11:59:40 -0300

    Continuação do desenvolvimento

--- servidor_nodejs_pre_configurado/app/models/JogoDAO.js
+++ servidor_nodejs_pre_configurado/app/models/JogoDAO.js
@@ -41,11 +41,11 @@ JogoDAO.prototype.acao = function(acao){
 
             var tempo = null;
 
-            switch(acao.acao){
-                case 1: tempo = 1 * 60 * 60000;
-                case 2: tempo = 2 * 60 * 60000;
-                case 3: tempo = 5 * 60 * 60000;
-                case 4: tempo = 5 * 60 * 60000;
+            switch(parseInt(acao.acao)){
+                case 1: tempo = 1 * 60 * 60000; break;
+                case 2: tempo = 2 * 60 * 60000; break;
+                case 3: tempo = 5 * 60 * 60000; break;
+                case 4: tempo = 5 * 60 * 60000; break;
             }
 
             acao.acao_termina_em = date.getTime() + tempo;
@@ -59,7 +59,11 @@ JogoDAO.prototype.acao = function(acao){
 JogoDAO.prototype.getAcoes = function(usuario, res){
     this._connection.open( function(err, mongoclient){
         mongoclient.collection("acao", function(err, collection){
-        collection.find({usuario: usuario}).toArray(function(err, result){
+
+            var date = new Date();
+            var momento_atual = date.getTime();
+
+        collection.find({usuario: usuario, acao_termina_em : {$gt:momento_atual}}).toArray(function(err, result){
         
         res.render("pergaminhos", {acoes: result}); 
 
