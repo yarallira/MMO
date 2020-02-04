f0548acf92b1ba682c2db051f8b9fed7de53d64d
cbykuser <yarallira555@gmail.com>
2020-02-04 14:51:45 -0300

    Continuação

--- servidor_nodejs_pre_configurado/app/models/JogoDAO.js
+++ servidor_nodejs_pre_configurado/app/models/JogoDAO.js
@@ -1,14 +1,14 @@
-function JogoDAO(connection){
+function JogoDAO(connection) {
     this._connection = connection();
 }
 // Yara
-JogoDAO.prototype.gerarParametros = function(usuario){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("jogo", function(err, collection){
+JogoDAO.prototype.gerarParametros = function (usuario) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("jogo", function (err, collection) {
             collection.insert({
-                usuario : usuario,
+                usuario: usuario,
                 moeda: 15,
-                suditos: 10, 
+                suditos: 10,
                 temor: Math.floor(Math.random() * 1000),
                 sabedoria: Math.floor(Math.random() * 1000),
                 comercio: Math.floor(Math.random() * 1000),
@@ -16,32 +16,32 @@ JogoDAO.prototype.gerarParametros = function(usuario){
             });
 
             mongoclient.close();
-    });
+        });
     });
 }
 
-JogoDAO.prototype.iniciaJogo = function(res, usuario, casa, msg){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("jogo", function(err, collection){
-        collection.find({usuario: usuario}).toArray(function(err, result){
-        res.render("jogo", {img_casa: casa, jogo: result[0], msg : msg});
+JogoDAO.prototype.iniciaJogo = function (res, usuario, casa, msg) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("jogo", function (err, collection) {
+            collection.find({ usuario: usuario }).toArray(function (err, result) {
+                res.render("jogo", { img_casa: casa, jogo: result[0], msg: msg });
 
-        mongoclient.close();
+                mongoclient.close();
 
-            });     
+            });
         });
     })
 }
 
-JogoDAO.prototype.acao = function(acao){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("acao", function(err, collection){
+JogoDAO.prototype.acao = function (acao) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("acao", function (err, collection) {
 
             var date = new Date();
 
             var tempo = null;
 
-            switch(parseInt(acao.acao)){
+            switch (parseInt(acao.acao)) {
                 case 1: tempo = 1 * 60 * 60000; break;
                 case 2: tempo = 2 * 60 * 60000; break;
                 case 3: tempo = 5 * 60 * 60000; break;
@@ -49,32 +49,49 @@ JogoDAO.prototype.acao = function(acao){
             }
 
             acao.acao_termina_em = date.getTime() + tempo;
-            collection.insert(acao);            
+            collection.insert(acao);
+          
+        });
 
-            mongoclient.close();
-    });
+        mongoclient.collection("jogo", function (err, collection) {
+
+            var moedas = null;
+            switch (parseInt(acao.acao)) {
+                case 1: moedas = -2 * acao.quantidade; break;
+                case 2: moedas = -3 * acao.quantidade; break;
+                case 3: moedas = -1 * acao.quantidade; break;
+                case 4: moedas = -1 * acao.quantidade; break;
+            }
+
+            collection.update(
+                {usuario: acao.usuario},
+                {$inc: {moeda: moedas}}
+                );
+
+                mongoclient.close();
+        });
     });
 }
 
-JogoDAO.prototype.getAcoes = function(usuario, res){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("acao", function(err, collection){
+JogoDAO.prototype.getAcoes = function (usuario, res) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("acao", function (err, collection) {
 
             var date = new Date();
             var momento_atual = date.getTime();
 
-        collection.find({usuario: usuario, acao_termina_em : {$gt:momento_atual}}).toArray(function(err, result){
-        
-        res.render("pergaminhos", {acoes: result}); 
+            collection.find({ usuario: usuario, acao_termina_em: { $gt: momento_atual } }).toArray(function (err, result) {
 
-        mongoclient.close();
+                res.render("pergaminhos", { acoes: result });
 
-            });     
+                mongoclient.close();
+
+            });
         });
     })
 
 }
 
-module.exports = function(){
+module.exports = function () {
     return JogoDAO;
 }
