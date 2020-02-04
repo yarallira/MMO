260ea62deb98703538d95feff084581440202a3f
cbykuser <yarallira555@gmail.com>
2020-02-04 15:49:35 -0300

    Desenvolvimento

--- servidor_nodejs_pre_configurado/app/controllers/jogo.js
+++ servidor_nodejs_pre_configurado/app/controllers/jogo.js
@@ -1,12 +1,12 @@
-module.exports.jogo = function(application , req, res){
-    
-    if(req.session.autorizado !== true){
+module.exports.jogo = function (application, req, res) {
+
+    if (req.session.autorizado !== true) {
         res.send('Usuário precisa fazer login');
         return;
     }
 
     var msg = '';
-    if(req.query.msg != ''){
+    if (req.query.msg != '') {
         msg = req.query.msg;
     }
 
@@ -16,42 +16,42 @@ module.exports.jogo = function(application , req, res){
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res, usuario, casa , msg);  
+    JogoDAO.iniciaJogo(res, usuario, casa, msg);
 
 }
 
-module.exports.sair = function(application , req, res){
-    
-  req.session.destroy( function(err){
-      res.render("index", {validacao: {}});
-  })
+module.exports.sair = function (application, req, res) {
+
+    req.session.destroy(function (err) {
+        res.render("index", { validacao: {} });
+    })
 }
 
-module.exports.suditos = function(application , req, res ){    
-    if(req.session.autorizado !== true){
+module.exports.suditos = function (application, req, res) {
+    if (req.session.autorizado !== true) {
         res.send('Usuário precisa fazer login');
         return;
     }
 
-        res.render("aldeoes", {validacao: {}});  
-  }
+    res.render("aldeoes", { validacao: {} });
+}
 
-  module.exports.pergaminhos = function(application , req, res ){  
-    if(req.session.autorizado !== true){
+module.exports.pergaminhos = function (application, req, res) {
+    if (req.session.autorizado !== true) {
         res.send('Usuário precisa fazer login');
         return;
     }
-          // recuperar as ações inseridas no banco de dados
-          var connection = application.config.dbConnection;
-          var JogoDAO = new application.app.models.JogoDAO(connection)
+    // recuperar as ações inseridas no banco de dados
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection)
 
-          var usuario = req.session.usuario;
+    var usuario = req.session.usuario;
 
-          JogoDAO.getAcoes(usuario, res);           
-  }
+    JogoDAO.getAcoes(usuario, res);
+}
 
-  module.exports.ordenar_acao_sudito = function(application , req, res ){    
-    if(req.session.autorizado !== true){
+module.exports.ordenar_acao_sudito = function (application, req, res) {
+    if (req.session.autorizado !== true) {
         res.send('Usuário precisa fazer login');
         return;
     }
@@ -61,19 +61,29 @@ module.exports.suditos = function(application , req, res ){
     req.assert('acao', 'Ação deve ser informada').notEmpty();
     req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
 
-   var erros = req.validationErrors();
+    var erros = req.validationErrors();
 
-   if(erros){
-       res.redirect('jogo?msg=A');
-       return;
-   }
-   
-   var connection = application.config.dbConnection;
-   var JogoDAO = new application.app.models.JogoDAO(connection);
+    if (erros) {
+        res.redirect('jogo?msg=A');
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
 
     dadosForm.usuario = req.session.usuario;
-   JogoDAO.acao(dadosForm);
-   
+    JogoDAO.acao(dadosForm);
 
-   res.redirect('jogo?msg=B');
+
+    res.redirect('jogo?msg=B');
+}
+
+module.exports.revogar_acao = function (application, req, res) {
+    var url_query = req.query;
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    var _id = url_query.id_acao
+    JogoDAO.revogarAcao(_id, res);
 }
--- servidor_nodejs_pre_configurado/app/models/JogoDAO.js
+++ servidor_nodejs_pre_configurado/app/models/JogoDAO.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 function JogoDAO(connection) {
     this._connection = connection();
 }
@@ -46,11 +48,12 @@ JogoDAO.prototype.acao = function (acao) {
                 case 2: tempo = 2 * 60 * 60000; break;
                 case 3: tempo = 5 * 60 * 60000; break;
                 case 4: tempo = 5 * 60 * 60000; break;
+                case 5: tempo = 1 * 60 * 60000; break;
             }
 
             acao.acao_termina_em = date.getTime() + tempo;
             collection.insert(acao);
-          
+
         });
 
         mongoclient.collection("jogo", function (err, collection) {
@@ -61,14 +64,15 @@ JogoDAO.prototype.acao = function (acao) {
                 case 2: moedas = -3 * acao.quantidade; break;
                 case 3: moedas = -1 * acao.quantidade; break;
                 case 4: moedas = -1 * acao.quantidade; break;
+                case 5: moedas = 20 * acao.quantidade; break;
             }
 
             collection.update(
-                {usuario: acao.usuario},
-                {$inc: {moeda: moedas}}
-                );
+                { usuario: acao.usuario },
+                { $inc: { moeda: moedas } }
+            );
 
-                mongoclient.close();
+            mongoclient.close();
         });
     });
 }
@@ -87,6 +91,21 @@ JogoDAO.prototype.getAcoes = function (usuario, res) {
                 mongoclient.close();
 
             });
+        });
+    })
+
+}
+
+JogoDAO.prototype.revogarAcao = function (_id, res) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("acao", function (err, collection) {
+            collection.remove(
+                { _id: ObjectID(_id) },
+                function (err, result) {
+                    res.redirect("jogo?msg=D")
+                    mongoclient.close();
+                }
+            )
         });
     })
 
