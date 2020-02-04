62a6aac7f6ad3672cf061695894708dd559005b6
cbykuser <yarallira555@gmail.com>
2020-02-04 16:40:56 -0300

    Desenvolvimento

--- servidor_nodejs_pre_configurado/app/models/UsuariosDAO.js
+++ servidor_nodejs_pre_configurado/app/models/UsuariosDAO.js
@@ -1,48 +1,60 @@
-function UsuariosDAO(connection){
+// Importar o módulo do Crypto
+var crypto = require('crypto');
+
+function UsuariosDAO(connection) {
     this._connection = connection();
 }
 
-UsuariosDAO.prototype.inserirUsuario = function(usuario){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("usuarios", function(err, collection){
+UsuariosDAO.prototype.inserirUsuario = function (usuario) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("usuarios", function (err, collection) {
+
+            var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest('hex');
+
+            usuario.senha = senha_criptografada;
+
             collection.insert(usuario);
 
             mongoclient.close();
+        });
     });
-});
 }
 
-UsuariosDAO.prototype.autenticar = function(usuario, req, res){
-    this._connection.open( function(err, mongoclient){
-        mongoclient.collection("usuarios", function(err, collection){
-            collection.find({usuario: {$eq: usuario.usuario}, senha: {$eq: usuario.senha}}).toArray(function(err, result){
+UsuariosDAO.prototype.autenticar = function (usuario, req, res) {
+    this._connection.open(function (err, mongoclient) {
+        mongoclient.collection("usuarios", function (err, collection) {
 
-    if(result[0] != undefined){
-        req.session.autorizado = true
+            var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest('hex');
+            usuario.senha = senha_criptografada;
 
-        req.session.usuario = result[0].usuario;
-        req.session.casa = result[0].casa;
-        }else{
-            req.session.autorizado = false
-        }
+            collection.find({ usuario: { $eq: usuario.usuario }, senha: { $eq: usuario.senha } }).toArray(function (err, result) {
 
-        if(req.session.autorizado){
-            res.redirect("jogo");
-            console.log(result);
-            console.log(req.session.autorizado);
-        }else{
-            res.render('index', {validacao: {}});
-            console.log(result);
-            console.log(req.session.autorizado);
-        }
+                if (result[0] != undefined) {
+                    req.session.autorizado = true
 
-     });
+                    req.session.usuario = result[0].usuario;
+                    req.session.casa = result[0].casa;
+                } else {
+                    req.session.autorizado = false
+                }
 
-    mongoclient.close();
-    });
-})
+                if (req.session.autorizado) {
+                    res.redirect("jogo");
+                    console.log(result);
+                    console.log(req.session.autorizado);
+                } else {
+                    res.render('index', { validacao: {} });
+                    console.log(result);
+                    console.log(req.session.autorizado);
+                }
+
+            });
+
+            mongoclient.close();
+        });
+    })
 }
 
-module.exports = function(){
+module.exports = function () {
     return UsuariosDAO;
 }
